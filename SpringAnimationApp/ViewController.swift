eade8a4df6d1f284cedcb9c7862eebde329dfd33
Anna Lavrova <anna.laurier99@gmail.com>
2024-03-06 13:06:25 +0300

    06.03

--- SpringAnimationApp/ViewController.swift
+++ SpringAnimationApp/ViewController.swift
@@ -5,16 +5,16 @@
 //  Created by Anna Lavrova on 05.03.2024.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var coreAnimationView: UIView!
 
     @IBOutlet weak var runCoreAnimationButton: UIButton!
-    @IBOutlet weak var springAnimationLabel: UILabel!
-    
-    @IBOutlet weak var springAnimationButton: UIButton!
+
+    @IBOutlet weak var springAnimationView: SpringView!
+    @IBOutlet weak var springAnimationButton: SpringButton!
 
     private var animationStarted = false
 
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
 
         coreAnimationView.layer.cornerRadius = 10
         runCoreAnimationButton.layer.cornerRadius = 10
-        springAnimationLabel.layer.cornerRadius = 10
+        springAnimationView.layer.cornerRadius = 10
         springAnimationButton.layer.cornerRadius = 10
     }
 
@@ -42,20 +42,16 @@ class ViewController: UIViewController {
             }
     }
 
-    @IBAction func runSpringAnimation(_ sender: UIButton) {
-        sender.pulsate()
-        
-        UIView.animate(
-            withDuration: 0.5,
-            delay: 0,
-            options: [.autoreverse, .repeat]) {
-                
-                if !self.animationStarted {
-                    self.coreAnimationView.frame.origin.x -= 40
-                    self.animationStarted.toggle()
-                }
-            }
-    }
+    @IBAction func runSpringAnimation(_ sender: SpringButton) {
+        springAnimationView.animation = "morph"
+        springAnimationView.curve = "easeIn"
+//        springAnimationView.force = 2
+        springAnimationView.duration = 1
 
+        springAnimationView.animate()
+
+        sender.animation = "wobble"
+        sender.animate()
+    }
 }
 
